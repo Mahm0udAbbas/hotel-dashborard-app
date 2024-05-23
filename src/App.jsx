b0eedbcdc9874da24c595421be5bea93b2392a88
Mahmoud Abbas <mahmoudabbas393@gmail.com>
2024-05-23 05:36:10 +0300

    add sidebar links

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ export default function App() {
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
-            <Route path="booking" element={<Bookings />} />
+            <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
-            <Route path="setting" element={<Settings />} />
+            <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
             <Route path="users" element={<NewUsers />} />
           </Route>
